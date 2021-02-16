f260b4c57f6838010ae201e619e96ec5c92f6986
YukiOchiai <xdi.grand.you.winner@gmail.com>
2021-02-16 17:34:37 +0900

    fixed router

--- src/routers/Router.ts
+++ src/routers/Router.ts
@@ -1,21 +1,26 @@
 import {Router} from "express";
 import KeyStatusController from "../controllers/KeyStatusController"
 
-const controller = new KeyStatusController()
-
 export const router: Router = Router()
 router.post('/fromLine',(req, res) => {
+
+  const controller = new KeyStatusController(res)
 
   const body = req.body
   const messageType: string = body.events[0].type
   const userId = body.events[0].source.userId
+
   if(messageType === "text"){ // messagePatternがTextの時
+
     const text = body.events[0].text
     controller.fromLineText(userId,text)
+
   }else if(messageType === "sticker"){ // messagePatternがstickerの時
+
     const packageId = body.events[0].packageId
     const stickerId = body.events[0].stickerId
     controller.fromLineSticker(userId,packageId,stickerId)
+
   }
 })
 
