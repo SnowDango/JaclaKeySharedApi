dae06b7fec7c73031f51b42d8c84e7028ea7f6fd
YukiOchiai <xdi.grand.you.winner@gmail.com>
2021-02-16 17:49:34 +0900

    fixed controller used model

--- src/controllers/KeyStatusController.ts
+++ src/controllers/KeyStatusController.ts
@@ -1,10 +1,11 @@
 import ResponseLine from "../responses/ResponseLine"
 import {Response} from "express";
+import LineModel from "../model/LineModel";
 
 export default class KeyStatusController {
 
   response: ResponseLine
-
+  model = new LineModel()
   constructor(res: Response) {
     this.response = new ResponseLine(res)
   }
@@ -12,13 +13,16 @@ export default class KeyStatusController {
   fromLineText = (userId: string, text: string) => {
 
     // TODO textの場合の処理
+    this.model.textModel(userId,text)
 
     this.response.successRes(200)
   }
 
-  fromLineSticker = (userId: string, packageId: string, sticker: string) => {
+  fromLineSticker = (userId: string, packageId: string, stickerId: string) => {
 
     // TODO stickerの場合の処理
+
+    this.model.stickerModel(userId,packageId,stickerId)
 
     this.response.successRes(200)
   }
