fd07c331e5a713f954faff22834b8fede2762da7
YukiOchiai <xdi.grand.you.winner@gmail.com>
2021-02-23 15:59:46 +0900

    fixed model test

--- test/model/LineModel.test.ts
+++ test/model/LineModel.test.ts
@@ -3,22 +3,32 @@ import {load} from "ts-dotenv";
 import MockDate from 'mockdate'
 import axios from "axios";
 
-const env = load({
-  LINE_USER_ID: String
-})
+const env = load({ LINE_USER_ID: String})
+const testDate = 1614007825481
+const testUser = "LineUserName"
+
+const model = new LineModel()
+
+const baseStatusFormat = (status:string): string => {
+  return `${testUser}が部室の鍵を${status}`
+}
+const baseTextFormat = (status:string): string => {
+  return ` user: LineUserName \n status: ${status} \n data: 2021/02/23(火) 00:30:25`
+}
+const notStatusText = 'not status'
+const notTextText = 'not text'
 
 describe('borrowed', () => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn( async () => {
-      return {status: 200,data:{displayName: "LineUserName"}}
+      return {status: 200,data:{displayName: testUser}}
     });
     const testText = "かり"
-    const model = new LineModel()
     const {baseStatus,baseText,twitterText} = await model.textModel(env.LINE_USER_ID,testText)
-    expect(baseStatus).toBe('LineUserNameが部室の鍵を借りました')
-    expect(baseText).toBe(' user: LineUserName \n status: 借りました \n data: 2021/02/23(火) 00:30:25')
+    expect(baseStatus).toBe(baseStatusFormat("借りました"))
+    expect(baseText).toBe(baseTextFormat("借りました"))
     expect(twitterText).toBe("")
   });
 })
@@ -26,15 +36,14 @@ describe('borrowed', () => {
 describe('opened', () => {
   afterEach( () => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn( async () => {
-      return {status: 200,data:{displayName: "LineUserName"}}
+      return {status: 200,data:{displayName: testUser}}
     });
     const testText = "開けました"
-    const model = new LineModel()
     const {baseStatus,baseText,twitterText} = await model.textModel(env.LINE_USER_ID,testText)
-    expect(baseStatus).toBe('LineUserNameが部室の鍵を開けました')
-    expect(baseText).toBe(' user: LineUserName \n status: 開けました \n data: 2021/02/23(火) 00:30:25')
+    expect(baseStatus).toBe(baseStatusFormat("開けました"))
+    expect(baseText).toBe(baseTextFormat("開けました"))
     expect(twitterText).toBe("開けました")
   });
 })
@@ -43,15 +52,14 @@ describe('opened', () => {
 describe('borrowed and opened', () => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn(async () => {
       return {status: 200, data: {displayName: "LineUserName"}}
     });
     const testText = "かりあけ"
-    const model = new LineModel()
     const {baseStatus, baseText,twitterText} = await model.textModel(env.LINE_USER_ID, testText)
-    expect(baseStatus).toBe('LineUserNameが部室の鍵を借りて開けました')
-    expect(baseText).toBe(' user: LineUserName \n status: 借りて開けました \n data: 2021/02/23(火) 00:30:25')
+    expect(baseStatus).toBe(baseStatusFormat("借りて開けました"))
+    expect(baseText).toBe(baseTextFormat("借りて開けました"))
     expect(twitterText).toBe("開けました")
   })
 })
@@ -59,15 +67,14 @@ describe('borrowed and opened', () => {
 describe('closed', () => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn(async () => {
-      return {status: 200, data: {displayName: "LineUserName"}}
+      return {status: 200, data: {displayName: testUser}}
     });
     const testText = "研A301の鍵をしめました"
-    const model = new LineModel()
     const {baseStatus, baseText,twitterText} = await model.textModel(env.LINE_USER_ID, testText)
-    expect(baseStatus).toBe('LineUserNameが部室の鍵を閉めました')
-    expect(baseText).toBe(' user: LineUserName \n status: 閉めました \n data: 2021/02/23(火) 00:30:25')
+    expect(baseStatus).toBe(baseStatusFormat("閉めました"))
+    expect(baseText).toBe(baseTextFormat("閉めました"))
     expect(twitterText).toBe("閉めました")
   })
 })
@@ -75,15 +82,14 @@ describe('closed', () => {
 describe('opened and closed', () => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn(async () => {
-      return {status: 200, data: {displayName: "LineUserName"}}
+      return {status: 200, data: {displayName: testUser}}
     });
     const testText = "あけてしめました"
-    const model = new LineModel()
     const {baseStatus, baseText,twitterText} = await model.textModel(env.LINE_USER_ID, testText)
-    expect(baseStatus).toBe('LineUserNameが部室の鍵を開けて閉めました')
-    expect(baseText).toBe(' user: LineUserName \n status: 開けて閉めました \n data: 2021/02/23(火) 00:30:25')
+    expect(baseStatus).toBe(baseStatusFormat("開けて閉めました"))
+    expect(baseText).toBe(baseTextFormat("開けて閉めました"))
     expect(twitterText).toBe("開けて閉めました")
   })
 })
@@ -91,15 +97,14 @@ describe('opened and closed', () => {
 describe('returned', () => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn(async () => {
-      return {status: 200, data: {displayName: "LineUserName"}}
+      return {status: 200, data: {displayName: testUser}}
     });
     const testText = "かえしました"
-    const model = new LineModel()
     const {baseStatus, baseText,twitterText} = await model.textModel(env.LINE_USER_ID, testText)
-    expect(baseStatus).toBe('LineUserNameが部室の鍵を返しました')
-    expect(baseText).toBe(' user: LineUserName \n status: 返しました \n data: 2021/02/23(火) 00:30:25')
+    expect(baseStatus).toBe(baseStatusFormat("返しました"))
+    expect(baseText).toBe(baseTextFormat("返しました"))
     expect(twitterText).toBe("")
   })
 })
@@ -107,15 +112,14 @@ describe('returned', () => {
 describe('closed and returned', () => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn(async () => {
       return {status: 200, data: {displayName: "LineUserName"}}
     });
     const testText = "しめかえし"
-    const model = new LineModel()
     const {baseStatus, baseText,twitterText} = await model.textModel(env.LINE_USER_ID, testText)
-    expect(baseStatus).toBe('LineUserNameが部室の鍵を閉めて返しました')
-    expect(baseText).toBe(' user: LineUserName \n status: 閉めて返しました \n data: 2021/02/23(火) 00:30:25')
+    expect(baseStatus).toBe(baseStatusFormat("閉めて返しました"))
+    expect(baseText).toBe(baseTextFormat("閉めて返しました"))
     expect(twitterText).toBe("閉めました")
   })
 })
@@ -123,15 +127,14 @@ describe('closed and returned', () => {
 describe('all status', () => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn(async () => {
-      return {status: 200, data: {displayName: "LineUserName"}}
+      return {status: 200, data: {displayName: testUser}}
     });
     const testText = "かりあけしめかえし"
-    const model = new LineModel()
     const {baseStatus, baseText,twitterText} = await model.textModel(env.LINE_USER_ID, testText)
-    expect(baseStatus).toBe('LineUserNameが部室の鍵を借りて開けて閉めて返しました')
-    expect(baseText).toBe(' user: LineUserName \n status: 借りて開けて閉めて返しました \n data: 2021/02/23(火) 00:30:25')
+    expect(baseStatus).toBe(baseStatusFormat("借りて開けて閉めて返しました"))
+    expect(baseText).toBe(baseTextFormat("借りて開けて閉めて返しました"))
     expect(twitterText).toBe("開けて閉めました")
   })
 })
@@ -139,47 +142,44 @@ describe('all status', () => {
 describe('random text', () => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn(async () => {
-      return {status: 200, data: {displayName: "LineUserName"}}
+      return {status: 200, data: {displayName: testUser}}
     });
     const testText = "今日21:00くらいからdiscordでamong usやるみたいなんで、やりたい方どうぞー\n" + "後輩大歓迎"
-    const model = new LineModel()
     const {baseStatus, baseText,twitterText} = await model.textModel(env.LINE_USER_ID, testText)
-    expect(baseStatus).toBe('not status')
-    expect(baseText).toBe('not text')
-    expect(twitterText).toBe('not text')
+    expect(baseStatus).toBe(notStatusText)
+    expect(baseText).toBe(notTextText)
+    expect(twitterText).toBe(notTextText)
   })
 })
 
 describe('take away after 10',() => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn(async () => {
-      return {status: 200, data: {displayName: "LineUserName"}}
+      return {status: 200, data: {displayName: testUser}}
     });
     const testText = "学務課に鍵を返せなかったので、のーだが持って帰ります"
-    const model = new LineModel()
     const {baseStatus, baseText,twitterText} = await model.textModel(env.LINE_USER_ID, testText)
-    expect(baseStatus).toBe('LineUserNameが鍵を持ち帰りました')
-    expect(baseText).toBe(' user: LineUserName \n status: 持ち帰りました \n data: 2021/02/23(火) 00:30:25')
-    expect(twitterText).toBe('not text')
+    expect(baseStatus).toBe(`${testUser}が鍵を持ち帰りました`)//部室の鍵か断定出来ない
+    expect(baseText).toBe(baseTextFormat("持ち帰りました"))
+    expect(twitterText).toBe(notTextText)
   })
 })
 
 describe('take away before 10', () => {
   afterEach(() => {jest.restoreAllMocks()})
   it('should status and text is base format', async () => {
-    MockDate.set(1614007825481);
+    MockDate.set(testDate);
     (axios.get as any) = jest.fn(async () => {
       return {status: 200, data: {displayName: "LineUserName"}}
     });
     const testText = "鍵持ち帰ります"
-    const model = new LineModel()
     const {baseStatus, baseText,twitterText} = await model.textModel(env.LINE_USER_ID, testText)
-    expect(baseStatus).toBe('LineUserNameが部室の鍵を持ち帰りました')
-    expect(baseText).toBe(' user: LineUserName \n status: 持ち帰りました \n data: 2021/02/23(火) 00:30:25')
+    expect(baseStatus).toBe(baseStatusFormat("持ち帰りました"))
+    expect(baseText).toBe(baseTextFormat("持ち帰りました"))
     expect(twitterText).toBe("")
   })
 })
